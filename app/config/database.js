15608c8517ce447bde1bbaae6c63635c3d644e74
rafiammar014 <rafiammar014@gmail.com>
2026-01-11 18:27:56 +0700

    database config

--- app/config/database.js
+++ app/config/database.js
@@ -1,1 +1,20 @@
 // TODO: Buat koneksi pool MySQL disini menggunakan Environment Variable (process.env)
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'db', // 'db' adalah nama service di docker-compose
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'rootpassword',
+    database: process.env.DB_NAME || 'hustlink_db',
+    multipleStatements: true
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('ERROR: Gagal konek ke database:', err);
+        return;
+    }
+    console.log('SUKSES: Terhubung ke Database MySQL');
+});
+
+module.exports = connection;
